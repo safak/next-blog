93b7c3f54a60e45916cf0d81934c9ea62ba4e960
Mrfoxc121 <cfox0115@gmail.com>
2023-09-05 10:06:37 +0100

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="">
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+
