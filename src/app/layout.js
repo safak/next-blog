a1c3053c29e3509c0aa7cb607691168dc3341493
Luke Barry <lukebarry100@gmail.com>
2023-09-13 15:17:29 +0100

    add basic styling and display components on page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,28 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: 'Blog App by Luke Barry',
+  description: 'The best blog app evar!',
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div classname="wrapper">
+            <Navbar/>
+            {children}
+            <Footer/>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/menu";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <Featured/>
+      <CategoryList/>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+      </div>
+    </div>
+  );
 }
