82a7008f8362c56f58972d88353a34a803b6cec5
tskhvedian <t.tskhvediani@gmail.com>
2023-12-01 16:45:13 +0400

    Layout responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,23 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Blog App",
+  description: "The best blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
 import styles from "./homepage.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href="/">
+      Home
+    </Link>
+  </div>
 }
