bf4366f5bb8ed9611f4b3b5c026ab6a402842cdd
tskhvedian <t.tskhvediani@gmail.com>
2023-12-01 21:55:26 +0400

    dark and light

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -6,11 +6,11 @@ import { ThemeContext } from '@/context/ThemeContext'
 
 const ThemeToggle = () => {
 
-  const {theme} = useContext(ThemeContext)
+  const {toggle, theme} = useContext(ThemeContext)
   console.log(theme)
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <Image src="/moon.png" alt="" width={14} height={14}  />
       <div className={styles.ball}></div>
       <Image src="/sun.png" alt="" width={14} height={14}  />
